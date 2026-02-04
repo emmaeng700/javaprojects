521af2e66425349a2d6e667448e4681881502c03
Emmanuel Oppong <eoppong@gsumail.gram.edu>
2026-02-04 13:24:53 -0600

    updating code structure

--- file-upload-app/backend/src/test/java/com/example/uploadingfiles/UploadingfilesApplicationTests.java
+++ file-upload-app/backend/src/test/java/com/example/uploadingfiles/UploadingfilesApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Import;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
-class UploadingFilesApplicationTests {
+class UploadingfilesApplicationTests {
 
 	@Test
 	void contextLoads() {
