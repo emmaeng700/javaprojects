355fa56899ef4b2bfeb7e5a66b6f30470e3612e8
Emmanuel Oppong <eoppong@gsumail.gram.edu>
2026-02-06 03:39:02 -0600

    chatapp to learn redis

---
+++ chatapp/redis-chat/src/test/java/com/example/redis_chat/RedisChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.redis_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
