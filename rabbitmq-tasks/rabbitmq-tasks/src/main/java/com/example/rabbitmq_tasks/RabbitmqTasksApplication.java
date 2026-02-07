ac4e0203deeaa62a24edb9cc36e6e07ee04929c8
Emmanuel Oppong <eoppong@gsumail.gram.edu>
2026-02-07 14:57:51 -0600

    simple app that publishes subscribes to a RabbitMQ AMQP server

--- rabbitmq-tasks/rabbitmq-tasks/src/main/java/com/example/rabbitmq_tasks/RabbitmqTasksApplication.java
+++ rabbitmq-tasks/rabbitmq-tasks/src/main/java/com/example/rabbitmq_tasks/RabbitmqTasksApplication.java
@@ -2,8 +2,10 @@ package com.example.rabbitmq_tasks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class RabbitmqTasksApplication {
 
 	public static void main(String[] args) {
