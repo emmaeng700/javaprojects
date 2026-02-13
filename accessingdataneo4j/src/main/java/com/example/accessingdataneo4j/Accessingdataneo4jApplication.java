e97f0973e9aa565b23de289446e86c8ad7d94813
Emmanuel Oppong <eoppong@gsumail.gram.edu>
2026-02-13 03:59:19 -0600

    final updates

---
+++ accessingdataneo4j/src/main/java/com/example/accessingdataneo4j/Accessingdataneo4jApplication.java
@@ -0,0 +1,13 @@
+package com.example.accessingdataneo4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Accessingdataneo4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Accessingdataneo4jApplication.class, args);
+	}
+
+}
---
+++ accessingdataneo4j/src/test/java/com/example/accessingdataneo4j/Accessingdataneo4jApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accessingdataneo4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Accessingdataneo4jApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
